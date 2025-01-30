26036ea878b40e594c0b0f693f4524844fb20427
maxy <166556889+MaxyTee@users.noreply.github.com>
2025-01-31 00:18:39 +0100

    Add files via upload

---
+++ code/script.js
@@ -0,0 +1,32 @@
+"use strict";
+console.log("Hello World");
+// function updateTime() {
+//   const utcTIme = new Date().toISOString();
+//   document.getElementById("utc-time").textContent = utcTIme;
+//   console.log(utcTIme);
+// }
+
+console.log("Hello");
+
+function updateTime() {
+  const now = new Date();
+  const options = {
+    hour: "numeric",
+    minute: "numeric",
+    day: "numeric",
+    month: "numeric",
+    month: "numeric",
+    year: "numeric",
+    // weekday: 'long',
+  };
+
+  // const locale = navigator.language;
+  // console.log(locale);
+
+  document.getElementById("utc-time").textContent = new Intl.DateTimeFormat(
+    navigator.language.locale,
+    options
+  ).format(now);
+}
+
+updateTime();
